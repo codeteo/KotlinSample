c46c11b4a85498df240fe188c80f68a49ce80275
teo <ramones.teo@gmail.com>
2017-08-29 22:55:27 +0300

    Injects SharedPreferences to MainActivity

--- app/src/main/java/com/kotlin/mykotlinsample/MainActivity.kt
+++ app/src/main/java/com/kotlin/mykotlinsample/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.kotlin.mykotlinsample
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import butterknife.bindView
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,9 +15,14 @@ class MainActivity : AppCompatActivity() {
     val tvIncrement: TextView by bindView(R.id.tv_increment)
     val btnHi: Button by bindView(R.id.btn_hi)
 
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (application as MyApplication).applicationComponent.inject(this)
 
         tvHello.text = resources.getString(R.string.tv_hello)
 
